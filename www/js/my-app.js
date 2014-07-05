8f63ba0b29d56052a145f6a11b9d04aff0b5a986
Marck Regio <marckregio@gmail.com>
2014-07-05 17:09:14 +0800

    Commit

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -3,8 +3,24 @@ var myApp = new Framework7();
 
 // Export selectors engine
 //var $$ = myApp.$;
-
 $(document).ready(function(){
-                  
+                  $('.requestPage').click(function(){
+                                          $('.page3').removeClass('hidden').addClass('shown');
+                                          if ($('.page2').hasClass('shown')){
+                                          $('.page2').removeClass('shown').addClass('hidden');
+                                          }
+                                          $('.backer').removeClass('hidden').addClass('shown');
+                                          $('.backer').addClass('toPage2');
+                                          });
+                  $('.backer').click(function(){
+                                     if($(this).hasClass('toPage2')){
+                                     $('.page2').removeClass('hidden').addClass('shown');
+                                        if ($('.page3').hasClass('shown')){
+                                        $('.page3').removeClass('shown').addClass('hidden');
+                                        }
+                                     $('.backer').animate().addClass('hidden').removeClass('shown');
+                                     $('.backer').removeClass('toPage2');
+                                     }
+                                     });
                   });
 
