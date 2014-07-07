d756f85d81808e4918e5d6777e2f8a38c139a29a
Marck Regio <marckregio@gmail.com>
2014-07-07 13:09:19 +0800

    Commit
    July 7, 2014 1:09pm

--- www/js/makunatAPI.js
+++ www/js/makunatAPI.js
@@ -1,7 +1,6 @@
 /*
  * Functions API by Marck Litonjua Regio
  */
-var id = Math.floor((Math.random() * 1000) + (Math.random() * 100));
 var db;
 var ret;
 function makoy(){
@@ -40,7 +39,18 @@ function createDB(){
     db.transaction(createTables, gotError, function(){ console.log("Db Created"); });
 }
 function createTables(tx){
-    tx.executeSql('Create Table If Not Exists AccountHandler (id unique, lastname text, firstname text, middlename text, birthdate text, mobileno text, address text)');
+    tx.executeSql('Create Table If Not Exists AccountHandler (id integer primary key autoincrement, lastname VARCHAR(255), firstname VARCHAR(255), middlename VARCHAR(255), birthdate VARCHAR(255), mobileno VARCHAR(255), address VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists MainTableActivityName (id integer primary key autoincrement, activityName VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists MainTableAccountNo(id integer primary key autoincrement, accountNumber VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists MainTableCostCenter (id integer primary key autoincrement, costCenter VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists PlaneTableAirline (id integer primary key autoincrement, airline VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists HotelTableLocation (id integer primary key autoincrement, location VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists HotelTableRoomType (id integer primary key autoincrement, roomType VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists HotelTableRoomCategory (id integer primary key autoincrement, roomCategory VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists CarTableDescription (id integer primary key autoincrement, description VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists CarTableDuration (id integer primary key autoincrement, duration VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists CarTableDetails (id integer primary key autoincrement, details VARCHAR(255))');
+    tx.executeSql('Create Table If Not Exists Passenger (id integer primary key autoincrement, name VARCHAR(255), mobile VARCHAR(255), address VARCHAR(255))');
 }
 function runSQL(queryString){
     //showAlert(queryString);
@@ -57,6 +67,54 @@ function runSQLReturn(dbName,fields,whereClause){
                        $('.page1').removeClass('hidden').addClass('shown');
                        $('.open-panel').addClass('hidden');
                        }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "Passenger"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getPassengerList, gotError);
+                       }, function(){ console.log("Cannot Load Passengers");
+                       }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "PlaneTableAirline"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getPlaneList, gotError);
+                       }, function(){ console.log("Cannot Load Plane List");
+                       }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "HotelTableLocation"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getLocationList, gotError);
+                       }, function(){ console.log("Cannot Load Plane List");
+                       }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "HotelTableRoomType"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getRoomTypeList, gotError);
+                       }, function(){ console.log("Cannot Load Plane List");
+                       }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "HotelTableRoomCategory"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getRoomCategoryList, gotError);
+                       }, function(){ console.log("Cannot Load Plane List");
+                       }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "CarTableDescription"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getCarDescriptionList, gotError);
+                       }, function(){ console.log("Cannot Load Plane List");
+                       }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "CarTableDuration"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getCarDurationList, gotError);
+                       }, function(){ console.log("Cannot Load Plane List");
+                       }, function(){ console.log("runSQLReturn Successful"); });
+    } else if( dbName == "CarTableDetails"){
+        queryString = "Select " + fields + " from " + dbName + whereClause;
+        db.transaction(function(tx){
+                       tx.executeSql(queryString,[], getCarDetailsList, gotError);
+                       }, function(){ console.log("Cannot Load Plane List");
+                       }, function(){ console.log("runSQLReturn Successful"); });
     }
     
 }
@@ -65,6 +123,67 @@ function getAccountList(tx, results){
     //showAlert("Welcome to E-Travel " + fullname);
     $('.page2').removeClass('hidden').addClass('shown');
     $('.requestorName').text(fullname);
+}
+function getPassengerList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['name'];
+        $('.planePassenger').append('<option value="' + dt + '">' + dt + '</option>');
+        $('.hotelGuest').append('<option value="' + dt + '">' + dt + '</option>');
+        $('.carPassenger').append('<option value="' + dt + '">' + dt + '</option>');
+        $('.hcpReg').append('<option value="' + dt + '">' + dt + '</option>');
+        $('.hcpOther').append('<option value="' + dt + '">' + dt + '</option>');
+        }
+}
+function getPlaneList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['airline'];
+        $('.preferredAirlineFlyin').append('<option value="' + dt + '">' + dt + '</option>');
+        $('.preferredAirlineFlyout').append('<option value="' + dt + '">' + dt + '</option>');
+    }
+}
+function getLocationList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['location'];
+        $('.hotelLocation').append('<option value="' + dt + '">' + dt + '</option>');
+    }
+}
+function getRoomTypeList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['roomType'];
+        $('.roomType').append('<option value="' + dt + '">' + dt + '</option>');
+    }
+}
+function getRoomCategoryList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['roomCategory'];
+        $('.roomCategory').append('<option value="' + dt + '">' + dt + '</option>');
+    }
+}
+function getCarDescriptionList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['description'];
+        $('.carDescription').append('<option value="' + dt + '">' + dt + '</option>');
+    }
+}
+function getCarDetailsList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['details'];
+        $('.carDetails').append('<option value="' + dt + '">' + dt + '</option>');
+    }
+}
+function getCarDurationList(tx, results){
+    var len = results.rows.length;
+    for (var i = 0; i < len; i++){
+        var dt = results.rows.item(i)['duration'];
+        $('.carDuration').append('<option value="' + dt + '">' + dt + '</option>');
+    }
 }
 //Error Handlers
 function showAlert(alert){
@@ -84,7 +203,16 @@ function gotError(error){
 function fireJquery(){
     $(document).ready(function(){
                       //runSQL("Delete from AccountHandler");
+                      //Load Maintenance
                       runSQLReturn("AccountHandler","*","");
+                      runSQLReturn("Passenger", "*", "");
+                      runSQLReturn("PlaneTableAirline", "*", "");
+                      runSQLReturn("HotelTableLocation","*","");
+                      runSQLReturn("HotelTableRoomType","*","");
+                      runSQLReturn("HotelTableRoomCategory","*","");
+                      runSQLReturn("CarTableDescription","*","");
+                      runSQLReturn("CarTableDetails","*","");
+                      runSQLReturn("CarTableDuration","*","");
                       $('.requestPage').click(function(){
                                               $('.page3').removeClass('hidden').addClass('shown');
                                               if ($('.page2').hasClass('shown')){
@@ -114,8 +242,8 @@ function fireJquery(){
                                              });
                       //RegisterUser
                       $('.newUserSubmit').click(function(){
-                                                runSQL("Insert Into AccountHandler (id, lastname, firstname, middlename, birthdate, mobileno, address) Values \
-                                                       ("+id+",'"+$('.lastname').val()+"','"+$('.firstname').val()+"','"+$('.middlename').val()+"', \
+                                                runSQL("Insert Into AccountHandler (lastname, firstname, middlename, birthdate, mobileno, address) Values \
+                                                       ('"+$('.lastname').val()+"','"+$('.firstname').val()+"','"+$('.middlename').val()+"', \
                                                        '"+$('.birthdate').val()+"','"+$('.mobile').val()+"','"+$('.address').val()+"')");
                                                 runSQLReturn("AccountHandler","*","");
                                                 });
